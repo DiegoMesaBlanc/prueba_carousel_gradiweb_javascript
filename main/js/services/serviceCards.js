e3a22245ae60c84ea8dcd0a131bb021909df4193
Diego Mesa <dhiego__07@hotmail.com>
2024-01-29 11:38:38 -0500

    Ajuste CORS con headers

--- main/js/services/serviceCards.js
+++ main/js/services/serviceCards.js
@@ -1,7 +1,12 @@
 export const getStore = async () => {
   const API = "https://gradistore-spi.herokuapp.com/products/all"
 
-  const response = await fetch(API, { mode: "no-cors" });
+  const response = await fetch(API, {
+    'mode': 'cors',
+    'headers': {
+      'Access-Control-Allow-Origin': '*',
+    }
+  });
   const data = await response.json();
 
   return data
