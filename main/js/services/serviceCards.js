db2a0d8f9a90c55fe5dc853dd294a420d5976cba
Diego Mesa <dhiego__07@hotmail.com>
2024-01-29 11:28:18 -0500

    Ajuste CORS

--- main/js/services/serviceCards.js
+++ main/js/services/serviceCards.js
@@ -1,7 +1,7 @@
 export const getStore = async () => {
   const API = "https://gradistore-spi.herokuapp.com/products/all"
 
-  const response = await fetch(API);
+  const response = await fetch(API, { mode: "no-cors" });
   const data = await response.json();
 
   return data
